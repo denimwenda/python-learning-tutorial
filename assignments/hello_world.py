47d56ade2af30c15178e282c386078e3c85cbda9
denimwenda <denimwendwa@gmail.com>
2023-07-27 23:49:58 +0300

    changes

--- assignments/hello_world.py
+++ assignments/hello_world.py
@@ -3,3 +3,6 @@ message = ("Hello World!")
 print(message)
 message = ("Hello Python Crash Course world!")
 print(message)
+
+name = "Denny"
+print("Hello, " + name.title() + " " + "would you like to learn Python" + "?")
