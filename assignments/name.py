1ac147ba160e72e4b794b525b15d39bb8a0bf48d
denimwenda <denimwendwa@gmail.com>
2023-07-27 23:18:48 +0300

    changes

---
+++ assignments/name.py
@@ -0,0 +1,12 @@
+name = "ada lovelace"
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+first_name = "Denny"
+last_name = "Mutua"
+full_name = first_name + " " + last_name
+print(full_name)
+print("Hello, " + full_name.title() + "!")
