8a1981544e9e72987dd13221bbf289412a4234ba
denimwenda <denimwendwa@gmail.com>
2023-07-27 23:41:34 +0300

    changes

---
+++ assignments/whitespace.py
@@ -0,0 +1,13 @@
+print("Languages: \nPython\nC\nJavascript")
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+#to remove whitespace on the right side
+favorite_language = ' Python '
+favorite_language.rstrip()
+#remove whitespace from left side
+favorite_language = ' Python '
+favorite_language.rstrip()
+#remove whitespace on both sides
+favorite_language = ' Python '
+favorite_language.strip()
+
+print(favorite_language)
