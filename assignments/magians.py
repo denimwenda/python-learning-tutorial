cf14c4ddc0ff463feace0eec9efc4101017e2a13
denimwenda <denimwendwa@gmail.com>
2023-08-01 10:08:24 +0300

    changes

---
+++ assignments/magians.py
@@ -0,0 +1,15 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+#doing more work with python
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great tick!")
+    print("I can`t wait to see the next tick, " + magician.title() + ".\n")
+
+print("Thank you, everyone. That was a great magic show!")
