0bddb0480429c87f2271589f2b0fedd796891b80
denimwenda <denimwendwa@gmail.com>
2023-08-04 23:00:52 +0300

    changes

---
+++ assignments/usernames.py
@@ -0,0 +1,14 @@
+usernames = ['denny', 'kenneth', 'admin', 'tony', 'sharon']
+for username in usernames:
+    print("Hello " + username + ".")
+
+usernames = ['denny', 'kenneth', 'admin', 'tony', 'sharon']
+if usernames == 'admin':
+    print("Hello" + username + ".")
+
+usernames = []
+if usernames:
+    for username in usernames:
+        print("Thank you, users")
+else:
+    print("We need to look for users")
