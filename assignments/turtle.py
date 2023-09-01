e0ee202c06d409739f953613e4b323b87f6dc488
denimwenda <denimwendwa@gmail.com>
2023-09-01 22:27:12 +0300

    changes

---
+++ assignments/turtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+# Create a turtle screen/window
+window = turtle.Screen()
+
+# Create a turtle object
+my_turtle = turtle.Turtle()
+
+# Move the turtle forward by 100 units
+my_turtle.forward(100)
+
+# Rotate the turtle to the right by 90 degrees
+my_turtle.right(90)
+
+# Move the turtle forward by 100 units again
+my_turtle.forward(100)
+
+# Close the turtle graphics window when clicked
+window.exitonclick()
