40a50dfe151a1f949e8f72a781a82fe1457da0cc
denimwenda <denimwendwa@gmail.com>
2024-09-30 23:33:08 +0300

    changes

---
+++ assignments/python.py
@@ -0,0 +1,19 @@
+# Taking input in python
+# Python code showing a use of input()
+val = input("Enter your value: ")
+print(val)
+
+
+name = input('What is your name?\n')
+print(name)
+
+# Program to check input
+# Tpe in python
+num = input("Enter number: ")
+print(num)
+name1 = input("Enter name: ")
+print(name1)
+
+# Printing type of input value
+print("type of number", type(num))
+print("type of name", type(name1))
