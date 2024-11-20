e2b1b55ee96c7d0d57fd8e885131ca05c6798608
denimwenda <denimwendwa@gmail.com>
2024-11-20 22:16:50 +0300

    changes

---
+++ assignments/exceptionhandling.py
@@ -0,0 +1,14 @@
+amount = 10000
+if(amount > 2999):
+    print("You eligible to purchase Dsa Self Paced.")
+
+marks = 10000
+a = marks / 2
+print(a)
+
+x = 5
+y = "hello"
+try:
+    z = x + y
+except TypeError:
+    print("Error: cannot add an int and a string")
